ad543e97d12af31863170118abf189b95ea8c413
Alexander Cornwall <alex.earl.cornwall@gmail.com>
2026-03-07 19:51:01 +0300

    Updated main.py: Added confs copy func.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from paths_info import POPULAR_CONFIGS
 
 def create_dir(path):
@@ -22,7 +23,7 @@ def info_file_existence() -> bool:
     if os.path.exists('./info.txt'):
         return True
 
-    ans = input('Would you like to create ./info.txt file for info storage? (Y/n)')
+    ans = input('Would you like to create ./info.txt file for info storage? (Y/n) ')
     if ans in " Yy":
         try:
             create_file('info.txt')
@@ -36,7 +37,7 @@ def config_dir_existence() -> bool:
     if os.path.exists('./Configs'):
         return True
 
-    ans = input('Would you like to create ./Configs/ dir for config storage? (Y/n)')
+    ans = input('Would you like to create ./Configs/ dir for config storage? (Y/n) ')
     if ans in " Yy":
         try:
             create_dir("Configs")
@@ -61,19 +62,39 @@ def search_for_popular_configs():
 
     return find_confs
 
-def main():
-    find_confs = search_for_popular_configs()
+def show_finded_confs(finded_confs):
     print('Найдено:')
-    for conf_info in find_confs:
+    for conf_info in finded_confs:
         print(f'{len(conf_info) - 1} конфиг-файлов для {conf_info[0]}:')
         for confs in conf_info[1:]:
             print(confs)
 
         print()
 
+def copying_confs(find_confs, config_dir=False):
+    if not config_dir:
+        raise FileNotFoundError('Dir ./Configs/ not found. Programm can create it. Reopen programm.')
 
+    for conf_info in find_confs:
+        for path in conf_info[1:]:
+            try:
+                shutil.copy(path, f"Configs/{os.path.basename(path)}")
+                print(f'Скопирован конфиг {path} в Configs/{os.path.basename(path)}')
+            except Exception as e:
+                print(f'Something went wrong {e}')
 
+def main(config_dir_ex, info_file_ex):
+    
+    popular_confs = search_for_popular_configs()
+    show_finded_confs(popular_confs)
+    
+    ans = input('Do you want to copy finded configs? (Y/n) ')
+    if ans in ' Yy':
+        copying_confs(popular_confs, config_dir=config_dir_ex)
+    
+   
 if __name__ == "__main__":
-    # main()
-    pass
+    d, f = check_for_needed()
+    main(d, f)
+    
 
