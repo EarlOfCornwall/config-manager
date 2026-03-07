dd3b296d826bda56e5936680316f7e6da22b4522
Alexander Cornwall <alex.earl.cornwall@gmail.com>
2026-03-07 18:47:40 +0300

    Updated GitIgnore, added main func, added module with info

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
- 
+import os
+import pathlib
+from paths_info import POPULAR_CONFIGS
+
+def clear(): 
+    os.system('clear')
+
+def search_for_popular_configs():
+    find_confs = []
+    for prog, info in POPULAR_CONFIGS.items():
+        prog_confs = [prog,] # 1 element - prog name
+        for file in info['files']:
+            for path in info['paths']:
+                expanded_path = os.path.expanduser(path)
+                full_path = os.path.join(expanded_path, file)
+                if os.path.exists(full_path): 
+                    prog_confs.append(os.path.join(full_path))
+        
+        find_confs.append(prog_confs)
+
+    return find_confs
+
+def main():
+    find_confs = search_for_popular_configs()
+    print('Найдено:')
+    for conf_info in find_confs:
+        print(f'{len(conf_info) - 1} конфиг-файлов для {conf_info[0]}:')
+        for confs in conf_info[1:]:
+            print(confs)
+
+        print()
+
+main()
+        
---
+++ paths_info.py
@@ -0,0 +1,23 @@
+POPULAR_CONFIGS = {
+    'bash': {
+        'files': ['.bashrc', '.bash_profile', '.bash_aliases'],
+        'paths': ['~'],
+        },
+    'zsh': {
+        'files': ['.zshrc', '.zprofile', '.zshenv'],
+        'paths': ['~'],
+        },
+    'git': {
+        'files': ['.gitconfig', '.gitignore_global'],
+        'paths': ['~', '~/.config', '~/.config/git/'],
+        },
+    'vim': {
+        'files': ['.vimrc', '.gvimrc'],
+        'paths': ['~', '~/.config', '~/.config/vim/']
+        },
+    'kitty': {
+        'files': ['kitty.conf'],
+        'paths': ['~/.config/kitty/']
+        }
+}
+
